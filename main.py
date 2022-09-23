078f047cbe9998a0bf59d6f3c61fbb700964b691
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-09-23 11:45:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,167 @@
+import random
+from ascii_art import *
+from outcome import *
+
+print("Are you up for a game of [R]ock, [P]aper, [Sc]issors, [L]izard or [Sp]ock?\n")
+
+user_choice = input("Ready? 3... 2... 1...\n").lower().capitalize()
+
+RPScLSp = ['R', 'P', 'Sc', 'L', 'Sp']
+
+# Rock
+if user_choice == 'R':
+    print("\nYou chose Rock!")
+    rock()
+    print("\nVS")
+    computer_choice = random.choice(RPScLSp)
+    if computer_choice == 'R':
+        rock()
+        print("\nComputer chose Rock!")
+        draw()
+    elif computer_choice == 'Sc':
+        scissors()
+        print("\nComputer chose Scissors!")
+        win()
+        r_sc()
+    elif computer_choice == 'L':
+        lizard()
+        print("\nComputer chose Lizard!")
+        win()
+        r_l()
+    elif computer_choice == 'P':
+        paper()
+        print("\nComputer chose Paper!")
+        lose()
+        p_r()
+    elif computer_choice == 'Sp':
+        spock()
+        print("\nComputer chose Spock!")
+        lose()
+        sp_r()
+
+# Paper
+elif user_choice == 'P':
+    print("\nYou chose Paper!")
+    paper()
+    print("\nVS")
+    computer_choice = random.choice(RPScLSp)
+    if computer_choice == 'R':
+        rock()
+        print("\nComputer chose Rock!")
+        win()
+        p_r()
+    elif computer_choice == 'Sc':
+        scissors()
+        print("\nComputer chose Scissors!")
+        lose()
+        sc_p()
+    elif computer_choice == 'L':
+        lizard()
+        print("\nComputer chose Lizard!")
+        lose()
+        l_p()
+    elif computer_choice == 'P':
+        paper()
+        print("\nComputer chose Paper!")
+        draw()
+    elif computer_choice == 'Sp':
+        spock()
+        print("\nComputer chose Spock!")
+        win()
+        p_sp()
+
+# Scissors
+elif user_choice == 'Sc':
+    print("\nYou chose Scissors!")
+    scissors()
+    print("\nVS")
+    computer_choice = random.choice(RPScLSp)
+    if computer_choice == 'R':
+        rock()
+        print("\nComputer chose Rock!")
+        lose()
+        r_sc()
+    elif computer_choice == 'Sc':
+        scissors()
+        print("\nComputer chose Scissors!")
+        draw()
+    elif computer_choice == 'L':
+        lizard()
+        print("\nComputer chose Lizard!")
+        win()
+        sc_l()
+    elif computer_choice == 'P':
+        paper()
+        print("\nComputer chose Paper!")
+        win()
+        sc_p()
+    elif computer_choice == 'Sp':
+        spock()
+        print("\nComputer chose Spock!")
+        lose()
+        sp_sc()
+
+# Lizard
+elif user_choice == 'L':
+    print("\nYou chose Lizard!")
+    lizard()
+    print("\nVS")
+    computer_choice = random.choice(RPScLSp)
+    if computer_choice == 'R':
+        rock()
+        print("\nComputer chose Rock!")
+        lose()
+        r_l()
+    elif computer_choice == 'Sc':
+        scissors()
+        print("\nComputer chose Scissors!")
+        lose()
+        sc_l()
+    elif computer_choice == 'L':
+        lizard()
+        print("\nComputer chose Lizard!")
+        draw()
+    elif computer_choice == 'P':
+        paper()
+        print("\nComputer chose Paper!")
+        win()
+        l_p()
+    elif computer_choice == 'Sp':
+        spock()
+        print("\nComputer chose Spock!")
+        win()
+        l_sp()
+
+# Spock
+elif user_choice == 'Sp':
+    print("\nYou chose Spock!")
+    spock()
+    print("\nVS")
+    computer_choice = random.choice(RPScLSp)
+    if computer_choice == 'R':
+        rock()
+        print("\nComputer chose Rock!")
+        win()
+        sp_r()
+    elif computer_choice == 'Sc':
+        scissors()
+        print("\nComputer chose Scissors!")
+        win()
+        sp_sc()
+    elif computer_choice == 'L':
+        lizard()
+        print("\nComputer chose Lizard!")
+        lose()
+        l_sp()
+    elif computer_choice == 'P':
+        paper()
+        print("\nComputer chose Paper!")
+        lose()
+        p_sp()
+    elif computer_choice == 'Sp':
+        spock()
+        print("\nComputer chose Spock!")
+        draw()
+
+else:
+    print("\n[Invalid input. Please rerun.]")
---
+++ outcome.py
@@ -0,0 +1,38 @@
+def win():
+    print("\nYou win!")
+
+def lose():
+    print("\nYou lose!")
+
+def draw():
+    print("\nIt's a draw!")
+
+def sc_p():
+    print('\n"Scissors cuts Paper"')
+
+def p_r():
+    print('\n"Paper covers Rock"')
+
+def r_l():
+    print('\n"Rock crushes Lizard"')
+
+def l_sp():
+    print('\n"Lizard poisons Spock"')
+
+def sp_sc():
+    print('\n"Spock smashes Scissors"')
+
+def sc_l():
+    print('\n"Scissors decapitates Lizard"')
+
+def l_p():
+    print('\n"Lizard eats Paper"')
+
+def p_sp():
+    print('\n"Paper disproves Spock"')
+
+def sp_r():
+    print('\n"Spock vaporizes Rock"')
+
+def r_sc():
+    print('\n"Rock crushes Scissors"')
