fa64244ef6c4c6abc91c2aff54539cf085dab069
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-09-25 18:26:36 +0000

    Edited line6

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from ascii_art import *
 from outcome import *
 
 tbbt()
-print("""\33[3m~Best played on desktop to view ASCII art~\33[0m
+print("""\33[1;3m---Best played on desktop in full screen to view ASCII art---\33[0m
+
 Are you up for a game of the classic \33[31m[R]ock\33[0m-\33[33m[P]aper\33[0m-\33[35m[Sc]issors\33[0m-\33[32m[L]izard\33[0m-\33[34m[Sp]ock\33[0m?\n""")
 
 user_choice = input("Ready?\n").lower().capitalize()
