8e52937ff69b5f873be6350ebd0fda0e1bdadf34
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-09-25 18:05:31 +0000

    Added ASNI colour codes to ASCII art

--- main.py
+++ main.py
@@ -4,167 +4,167 @@ from outcome import *
 
 tbbt()
 print("""\33[3m~Best played on desktop to view ASCII art~\33[0m
-Are you up for a game of the classic [R]ock-[P]aper-[Sc]issors-[L]izard-[Sp]ock?\n""")
+Are you up for a game of the classic \33[31m[R]ock\33[0m-\33[33m[P]aper\33[0m-\33[35m[Sc]issors\33[0m-\33[32m[L]izard\33[0m-\33[34m[Sp]ock\33[0m?\n""")
 
 user_choice = input("Ready?\n").lower().capitalize()
 
-print("\nRock-Paper-Scissors-Lizard-Spock!")
+print("\n\33[31mRock\33[0m-\33[33mPaper\33[0m-\33[35mScissors\33[0m-\33[32mLizard\33[0m-\33[34mSpock\33[0m!")
 
 RPScLSp = ['R', 'P', 'Sc', 'L', 'Sp']
 
 # Rock
 if user_choice == 'R':
-    print("\nYou chose Rock!")
+    print("\nYou chose \33[31mRock\33[0m!")
     rock()
     print("\nVS")
     computer_choice = random.choice(RPScLSp)
     if computer_choice == 'R':
         rock()
-        print("\nComputer chose Rock!")
+        print("\nComputer chose \33[31mRock\33[0m!")
         draw()
     elif computer_choice == 'Sc':
         scissors()
-        print("\nComputer chose Scissors!")
+        print("\nComputer chose \33[35mScissors\33[0m!")
         win()
         r_sc()
     elif computer_choice == 'L':
         lizard()
-        print("\nComputer chose Lizard!")
+        print("\nComputer chose \33[32mLizard\33[0m!")
         win()
         r_l()
     elif computer_choice == 'P':
         paper()
-        print("\nComputer chose Paper!")
+        print("\nComputer chose \33[33mPaper\33[0m!")
         lose()
         p_r()
     elif computer_choice == 'Sp':
         spock()
-        print("\nComputer chose Spock!")
+        print("\nComputer chose \33[34mSpock\33[0m!")
         lose()
         sp_r()
 
 # Paper
 elif user_choice == 'P':
-    print("\nYou chose Paper!")
+    print("\nYou chose \33[33mPaper\33[0m!")
     paper()
     print("\nVS")
     computer_choice = random.choice(RPScLSp)
     if computer_choice == 'R':
         rock()
-        print("\nComputer chose Rock!")
+        print("\nComputer chose \33[31mRock\33[0m!")
         win()
         p_r()
     elif computer_choice == 'Sc':
         scissors()
-        print("\nComputer chose Scissors!")
+        print("\nComputer chose \33[35mScissors\33[0m!")
         lose()
         sc_p()
     elif computer_choice == 'L':
         lizard()
-        print("\nComputer chose Lizard!")
+        print("\nComputer chose \33[32mLizard\33[0m!")
         lose()
         l_p()
     elif computer_choice == 'P':
         paper()
-        print("\nComputer chose Paper!")
+        print("\nComputer chose \33[33mPaper\33[0m!")
         draw()
     elif computer_choice == 'Sp':
         spock()
-        print("\nComputer chose Spock!")
+        print("\nComputer chose \33[34mSpock!\33[0m")
         win()
         p_sp()
 
 # Scissors
 elif user_choice == 'Sc':
-    print("\nYou chose Scissors!")
+    print("\nYou chose \33[35mScissors\33[0m!")
     scissors()
     print("\nVS")
     computer_choice = random.choice(RPScLSp)
     if computer_choice == 'R':
         rock()
-        print("\nComputer chose Rock!")
+        print("\nComputer chose \33[31mRock\33[0m!")
         lose()
         r_sc()
     elif computer_choice == 'Sc':
         scissors()
-        print("\nComputer chose Scissors!")
+        print("\nComputer chose \33[35mScissors\33[0m!")
         draw()
     elif computer_choice == 'L':
         lizard()
-        print("\nComputer chose Lizard!")
+        print("\nComputer chose \33[32mLizard\33[0m!")
         win()
         sc_l()
     elif computer_choice == 'P':
         paper()
-        print("\nComputer chose Paper!")
+        print("\nComputer chose \33[33mPaper\33[0m!")
         win()
         sc_p()
     elif computer_choice == 'Sp':
         spock()
-        print("\nComputer chose Spock!")
+        print("\nComputer chose \33[34mSpock\33[0m!")
         lose()
         sp_sc()
 
 # Lizard
 elif user_choice == 'L':
-    print("\nYou chose Lizard!")
+    print("\nYou chose \33[32mLizard\33[0m!")
     lizard()
     print("\nVS")
     computer_choice = random.choice(RPScLSp)
     if computer_choice == 'R':
         rock()
-        print("\nComputer chose Rock!")
+        print("\nComputer chose \33[31mRock\33[0m!")
         lose()
         r_l()
     elif computer_choice == 'Sc':
         scissors()
-        print("\nComputer chose Scissors!")
+        print("\nComputer chose \33[35mScissors\33[0m!")
         lose()
         sc_l()
     elif computer_choice == 'L':
         lizard()
-        print("\nComputer chose Lizard!")
+        print("\nComputer chose \33[32mLizard\33[0m!")
         draw()
     elif computer_choice == 'P':
         paper()
-        print("\nComputer chose Paper!")
+        print("\nComputer chose \33[33mPaper\33[0m!")
         win()
         l_p()
     elif computer_choice == 'Sp':
         spock()
-        print("\nComputer chose Spock!")
+        print("\nComputer chose \33[34mSpock\33[0m!")
         win()
         l_sp()
 
 # Spock
 elif user_choice == 'Sp':
-    print("\nYou chose Spock!")
+    print("\nYou chose \33[34mSpock\33[0m!")
     spock()
     print("\nVS")
     computer_choice = random.choice(RPScLSp)
     if computer_choice == 'R':
         rock()
-        print("\nComputer chose Rock!")
+        print("\nComputer chose \33[31mRock\33[0m!")
         win()
         sp_r()
     elif computer_choice == 'Sc':
         scissors()
-        print("\nComputer chose Scissors!")
+        print("\nComputer chose \33[35mScissors\33[0m!")
         win()
         sp_sc()
     elif computer_choice == 'L':
         lizard()
-        print("\nComputer chose Lizard!")
+        print("\nComputer chose \33[32mLizard\33[0m!")
         lose()
         l_sp()
     elif computer_choice == 'P':
         paper()
-        print("\nComputer chose Paper!")
+        print("\nComputer chose \33[33mPaper\33[0m!")
         lose()
         p_sp()
     elif computer_choice == 'Sp':
         spock()
-        print("\nComputer chose Spock!")
+        print("\nComputer chose \33[34mSpock\33[0m!")
         draw()
 
 else:
